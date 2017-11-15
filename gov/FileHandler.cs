eb29e46e555ff3bb660c8d0f29dd80c21468cb28
FrozenBird <260404579@qq.com>
2017-11-15 16:00:02 +0800

    自动处理极速验证码进行验证进入系统采集企业信用信息。 http://www.gsxt.gov.cn/index.html
    自动处理极速验证码进行验证进入系统采集企业信用信息。 http://www.gsxt.gov.cn/index.html

---
+++ gov/FileHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace gov
+{
+    class FileHandler
+    {
+        public static void DeleteFilesIfExistsDir(string path)
+        {
+            if(!Directory.Exists(path))
+                return;
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileSystemInfo[] fileinfo = dir.GetFileSystemInfos();  //返回目录中所有文件和子目录
+            foreach (FileSystemInfo i in fileinfo)
+            {
+                if (i is DirectoryInfo)            //判断是否文件夹
+                {
+                    DirectoryInfo subdir = new DirectoryInfo(i.FullName);
+                    subdir.Delete(true);          //删除子目录和文件
+                }
+                else
+                {
+                    File.Delete(i.FullName);      //删除指定文件
+                }
+            }
+        }
+    }
+}
